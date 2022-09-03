fbc6c69d873b375c355192c7355853ad14994b22
valohaven <alissaapk@gmail.com>
2022-09-03 15:40:29 +0530

    trying with debug set to true

--- stradaproject/settings.py
+++ stradaproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)d2huvd3!=_13iq7$_@kj61kr3x)^^b56l)dfc6s=_m*@+2m!%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['strada1949.herokuapp.com', '127.0.0.1', 'strada1949.studio']
 
